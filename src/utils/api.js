7d27d508322f8f7291aeca13c136d0a605dd302c
Adeemma02 <emm.bolu@gmail.com>
2026-07-08 08:18:54 +0100

    new

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // src/utils/api.js
 
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_URL || "https://fosquizappback.onrender.com/api";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
